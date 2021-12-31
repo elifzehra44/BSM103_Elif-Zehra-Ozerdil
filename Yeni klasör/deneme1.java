44eb56ba0d0efdce3fbd0d2e9a4a025f283ee1d8
elifzehra44 <21010310027@ogrenci.bartin.edu.tr>
2021-12-31 16:31:03 +0300

    uygulama

---
+++ Yeni klasör/deneme1.java
@@ -0,0 +1,32 @@
+package uygulama1;
+
+public class deneme1 {
+
+	public static void main(String[] args) {
+		for(int a=0;a<5;a++){
+		    // Döngünün beþkere dönmesini saðlayan döngü.
+		    // a bir tamsayý ve 0 deðerine eþit. 
+		    // Döngü a, 5'den küçük olduðu sürece döngü tekrar edecek. 
+		    // a sayýsý her tekrarda 1 arttýrýlacak.
+
+		    for(int b=0;b<=a;b++){
+		    // Kaçýncý satýr ise o kadar dönen döngü.
+		    // Örneðin : ikinci satýrda ikikere dönecek.
+		    // b bir tamsayý ve 0 deðerine eþit. 
+		    // Döngü b, a'ya eþit ya da a'dan küçük olduðu sürece döngü tekrar edecek. 
+		    // b sayýsý her tekrarda 1 arttýrýlacak.
+		    
+		            System.out.print("*");
+		            // Ekrana " * " iþareti yazdýrýlacak.
+		            
+		        }
+		System.out.print("\n");
+		// Yeni bir satýra geçilecek
+
+		    }
+		  }
+		}
+		
+	
+
+
