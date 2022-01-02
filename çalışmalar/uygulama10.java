3c7aaa1a390e630bcf4d4e3d054f0f460046eae4
elifzehra44 <21010310027@ogrenci.bartin.edu.tr>
2022-01-02 14:09:32 +0300

    uygulama

---
+++ çalışmalar/uygulama10.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class uygulama10 {
+
+	public static void main(String[] args) {
+		
+		// çift sayıların oranı 1 2 3 4 => %50 çift
+		// 3 3 3 4 => %25 çift
+		// [1 2 3 4 ]
+		// 0 1 2 3
+		// array[0] = 3;
+		// array[2] = 4;
+		Scanner sc = new Scanner(System.in);
+		int[]array= new int[4];
+		System.out.println(array.length);
+		    for (int counter = 0; counter < array.length; counter++) {
+		    	System.out.print("Lütfen sayi giriniz:");
+		    	array[counter]=sc.nextInt();
+		}
+		    for (int counter = 0; counter < array.length; counter++) {
+		    	System.out.println("Dizinin elemanı:" + array[counter]);
+		
+	}
+}
+}
