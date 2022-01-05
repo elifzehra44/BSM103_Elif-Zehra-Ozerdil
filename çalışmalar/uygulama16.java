34b965ca1cb5a9be3ebbbcfdff51adb000a6c2d9
elifzehra44 <21010310027@ogrenci.bartin.edu.tr>
2022-01-05 22:25:58 +0300

    uygulama

---
+++ çalışmalar/uygulama16.java
@@ -0,0 +1,22 @@
+
+public class uygulama16 {
+
+	public static void main(String[] args) {
+		String isim = "Merhaba";
+		System.out.println(isim.length());
+		
+		System.out.println(isim.toLowerCase());
+		
+		System.out.println(isim.toUpperCase());
+		String ad = "Elif Zehra";
+        String soyad = "Özerdil";
+        
+        System.out.println(ad + " " + soyad);
+        
+        String x = "10";
+        String y = "20";
+        String z= x + y;
+        System.out.println(z);
+	}
+
+}
